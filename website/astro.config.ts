481fef7e37584cbb4c96b971f6eb561c8f00cd97
Lucas Barbosa <lucas.brsa@hotmail.com>
2026-06-28 15:12:36 +1000

    fixing cloudflare deploy under veracity.lbxa.net

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vla.lbxa.net",
+  site: "https://veracity.lbxa.net",
 
   prefetch: {
     prefetchAll: true,
